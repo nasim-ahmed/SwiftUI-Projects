e06f32739273ec400b121fbda3eca8646b3d53c8
Nasim Ahmed <nasim.ahmed54@gmail.com>
2022-08-10 11:32:49 +0200

    app storeanimation project initialized

---
+++ AppStoreAnimation/AppStoreAnimation/AppStoreAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreAnimationApp.swift
+//  AppStoreAnimation
+//
+//  Created by Nasim Ahmed on 08.08.22.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
