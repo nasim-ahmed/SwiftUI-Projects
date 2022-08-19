407a77642ab1e4b81b8d192bf4807b0ba762d239
Nasim Ahmed <nasim.ahmed54@gmail.com>
2022-08-19 14:17:24 +0200

    Created App gesture animation Project

---
+++ DragGestureAnimation/DragGestureAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DragGestureAnimation
+//
+//  Created by Nasim Ahmed on 19.08.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DragGestureAnimation/DragGestureAnimation/DragGestureAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragGestureAnimationApp.swift
+//  DragGestureAnimation
+//
+//  Created by Nasim Ahmed on 19.08.22.
+//
+
+import SwiftUI
+
+@main
+struct DragGestureAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
