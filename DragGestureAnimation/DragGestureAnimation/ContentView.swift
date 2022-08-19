1e6a533f026f113c34bcb75994ae23054140e66b
Nasim Ahmed <nasim.ahmed54@gmail.com>
2022-08-19 14:25:53 +0200

    added drag gesture to a rectangle

--- DragGestureAnimation/DragGestureAnimation/ContentView.swift
+++ DragGestureAnimation/DragGestureAnimation/ContentView.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var dragAmount = CGSize.zero
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .top, endPoint: .bottom)
+            .frame(width: 300, height: 200)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged({  dragAmount = $0.translation})
+                    .onEnded({ _ in
+                        dragAmount = CGSize.zero
+                    })
+            )
+            
+        
     }
 }
 
