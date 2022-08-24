bf88465cd912c55efde61c7ad313fbec843b5bd0
Nasim Ahmed <nasim.ahmed54@gmail.com>
2022-08-24 15:27:42 +0200

    added custom tabs

---
+++ Grids-TabView/Grids-TabView/Model/Feed.swift
@@ -0,0 +1,35 @@
+//
+//  Feed.swift
+//  Grids-TabView
+//
+//  Created by Nasim Ahmed on 24.08.22.
+//
+
+import Foundation
+
+struct Feed: Identifiable{
+    let id = UUID()
+    let name:String
+    let value: String
+    let image: String
+    let suggest: String
+}
+
+
+let dayFeeds = [
+    Feed(name: "Heart Rate", value: "124 BPM", image: "heart", suggest: "80-120 \nHealthy"),
+    Feed(name: "Sleep", value: "6h 43m", image: "moon.zzz", suggest: "Deep Sleep \n5h 13m"),
+    Feed(name: "Energy Burn", value: "583 kcal", image: "flame.fill", suggest: "900 \n900kcal"),
+    Feed(name: "Steps", value: "16741", image: "figure.walk", suggest: "Daily Goal \n20000 Steps"),
+    Feed(name: "Running", value: "5.3 km", image: "figure.run", suggest: "Daily Goal \n10km"),
+    Feed(name: "Cycling", value: "15.3 km", image: "figure.outdoor.cycle", suggest: "Daily Goal \n20km")
+]
+
+let weekFeeds = [
+    Feed(name: "Heart Rate", value: "84 BPM", image: "heart", suggest: "80-120 \nHealthy"),
+    Feed(name: "Sleep", value: "43h 23m", image: "moon.zzz", suggest: "Deep Sleep \n5h 13m"),
+    Feed(name: "Energy Burn", value: "3500 kcal", image: "flame.fill", suggest: "Weekly Goal \n4800kcal"),
+    Feed(name: "Steps", value: "17,8741", image: "figure.walk", suggest: "Weekly Goal \n25,0000 Steps"),
+    Feed(name: "Running", value: "45.3 km", image: "figure.run", suggest: "Weekly Goal \n70km"),
+    Feed(name: "Cycling", value: "100.3 km", image: "figure.outdoor.cycle", suggest: "Weekly Goal \n125km")
+]
--- Grids-TabView/Grids-TabView/View/HomeView.swift
+++ Grids-TabView/Grids-TabView/View/HomeView.swift
@@ -8,13 +8,67 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var index = 0
+    
     var body: some View {
-        Text("HomeView")
+        NavigationView{
+            VStack{
+                //Tab View
+                HStack(spacing: 0){
+                    Text("Day")
+                        .foregroundColor(index == 0 ?  Color.white: Color("TabColor"))
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 25)
+                        .background(index == 0 ? Color("TabBackground"): Color.white)
+                        .clipShape(Capsule())
+                        .onTapGesture {
+                            index = 0
+                        }
+                    Spacer(minLength: 0)
+                    
+                    Text("Weak")
+                        .foregroundColor(index == 1 ? Color.white: Color("TabColor"))
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 25)
+                        .background(index == 1 ? Color("TabBackground"): Color.white)
+                        .clipShape(Capsule())
+                        .onTapGesture {
+                            index = 1
+                        }
+                    Spacer(minLength: 0)
+                    
+                    Text("Month")
+                        .foregroundColor(index == 2 ? Color.white: Color("TabColor"))
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 25)
+                        .background(index == 2 ? Color("TabBackground"): Color.white)
+                        .clipShape(Capsule())
+                        .onTapGesture {
+                            index = 2
+                        }
+                }
+                .padding(.horizontal)
+                .clipShape(Capsule())
+                
+                //Dashboard Grid
+                
+                Spacer(minLength: 0)
+            }
+            .navigationTitle("STATS")
+        }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .previewDevice("iPhone 12 mini")
+            
     }
 }
